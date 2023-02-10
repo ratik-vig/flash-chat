7300c7686160df411d1cf17d840b25e30df20cda
Ratik Vig <ratikvig@gmail.com>
2023-02-09 23:36:17 -0500

    initial commit for web client

--- server/controllers/ConversationController.js
+++ server/controllers/ConversationController.js
@@ -8,10 +8,6 @@ const queries = require('../utils/queries')
 const router = express()
 router.use(express.json())
 
-router.get('/', (req, res) => {
-    res.status(200).send('conversation home route')
-})
-
 router.post('/create', auth, conversationValidators.createConversation, (req, res) => {
     const {user1, user2} = req.body
     db.query(queries.createConversation, [user1, user2, Date.now().toISOString()], (err, result) => {
